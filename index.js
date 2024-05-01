43e7256b6a058c4c70edc8e2145d3014749a696d
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-05-02 00:52:12 +0900

    Base

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}...`);
+})
