63abff5626d34d1b1a903e81d19a3034c4c924f2
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-05-08 14:01:06 +0900

    Circuit 3D

---
+++ models/Circuit.js
@@ -0,0 +1,9 @@
+const three = require('three');
+
+three.importmap = {
+  "imports": {
+    "three": "https://unpkg.com/three@0.138.3/build/three.module.js",
+    "GLTFLoader":
+    "https://unpkg.com/three@0.141.0/examples/jsm/loaders/GLTFLoader.js"
+  }
+}
