9500a6d7901c9a15e25eef4c126dbbe847eba213
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-05-10 16:38:39 +0900

    file clean

--- static/style/style.js
+++ static/style/style.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Menu start
-  // document
-  //   .querySelector(".menu-icon-box")
-  //   .addEventListener("click", function () {
-  //     document.querySelector(".menu-items").classList.toggle("show");
-  //     document
-  //       .querySelector(".menu-icon")
-  //       .classList.toggle("bi-menu-button-fill");
-  //     document.querySelector(".menu-icon").classList.toggle("bi-menu-button");
-  //   });
+  document
+    .querySelector(".menu-icon-box")
+    .addEventListener("click", function () {
+      document.querySelector(".menu-items").classList.toggle("show");
+      document
+        .querySelector(".menu-icon")
+        .classList.toggle("bi-menu-button-fill");
+      document.querySelector(".menu-icon").classList.toggle("bi-menu-button");
+    });
 
   document.addEventListener("click", function (event) {
     const menuItems = document.querySelector(".menu-items");
@@ -163,37 +163,3 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   // Login, register end
 });
-
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
-});
-// Menu end
-
-// Circuit banner start
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement); // 렌더러를 body에 추가
-
-// STLLoader를 사용하여 STL 파일을 불러옵니다.
-import * as THREE from 'three';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-var loader = new STLLoader();
-loader.load('../../src/3D/CircuitDeMonaco.stl', function (geometry) {
-    var material = new THREE.MeshNormalMaterial();
-    var mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-});
-
-camera.position.z = 5;
-
-// 렌더링 함수 정의
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-animate(); // 렌더링 시작
-// Circuit banner end
